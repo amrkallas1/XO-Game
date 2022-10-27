e6556cf9aa8db9f4fb536ff49d9ad73d83343f28
HP <amrkallas8@gmail.com>
2022-10-27 21:11:05 +0300

    F

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ let title = document.querySelector(".title span");
 let turn = document.querySelectorAll(".turn span");
 let boxes = document.querySelectorAll(".box");
 
-let winArray=[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[1,5,9],[2,5,8],[3,6,9],[3,5,7]];
-let winx=[];
-let x=0;
-let wino=[];
-let o=0;
-let end=0,xo=0;
+let winArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [1, 5, 9],
+  [2, 5, 8],
+  [3, 6, 9],
+  [3, 5, 7],
+];
+let winx = [];
+let x = 0;
+let wino = [];
+let o = 0;
+let end = 0,
+  xo = 0;
 let player = document.createElement("span");
 function selectUser() {
   turn.forEach((ele) => {
@@ -25,73 +35,69 @@ function play(id) {
     box.addEventListener("click", () => {
       if (box.innerHTML == "") {
         box.innerHTML = player;
-        box.style.pointerEvents="none";
+        box.style.pointerEvents = "none";
         box.classList.add(player);
         box.classList.add("end");
         end++;
-      if (player == "X") {
-            player = "O";
-            title.innerHTML = player;
-      } else {
-            player = "X";
-            title.innerHTML = player;
-      }
-    }
-    if(box.classList.contains("X")){
-        winx.push(parseInt(box.dataset.str))
-        if(winx.length>=3){
-            for(let i=0;i<winArray.length;i++){
-                for(let j=0;j<winArray[i].length;j++){
-                    for(let k=0;k<winx.length;k++){
-                        if(winx[k]==winArray[i][j]){
-                            x++;
-                        }
-                    }
-                }
-                if(x==3){
-                    document.querySelector(".title").innerHTML=`<span>X</span> Winner`;
-                    document.body.style.pointerEvents="none";
-                    xo=x;
-                }else{
-                    x=0
-                }
-            }
+        if (player == "X") {
+          player = "O";
+          title.innerHTML = player;
+        } else {
+          player = "X";
+          title.innerHTML = player;
         }
-    }
-
-    else if(box.classList.contains("O")){
-        wino.push(parseInt(box.dataset.str))
-        if(wino.length>=3){
-            for(let i=0;i<winArray.length;i++){
-                for(let j=0;j<winArray[i].length;j++){
-                    for(let k=0;k<wino.length;k++){
-                        if(wino[k]==winArray[i][j]){
-                            o++;
-                        }
-                    }
+      }
+      if (box.classList.contains("X")) {
+        winx.push(parseInt(box.dataset.str));
+        if (winx.length >= 3) {
+          for (let i = 0; i < winArray.length; i++) {
+            for (let j = 0; j < winArray[i].length; j++) {
+              for (let k = 0; k < winx.length; k++) {
+                if (winx[k] == winArray[i][j]) {
+                  x++;
                 }
-                if(o==3){
-                    document.querySelector(".title").innerHTML=`<span>O</span> Winner`;
-                    document.body.style.pointerEvents="none";
-                    xo=o
-                }else{
-                    o=0
-                }
+              }
             }
-        }   
-    }
-    if(end==9 &&xo!=3){
-        document.querySelector(".title").innerHTML=`<span>There is no winner</span>`;
-    }
+            if (x == 3) {
+              document.querySelector(
+                ".title"
+              ).innerHTML = `<span>X</span> Winner`;
+              document.body.style.pointerEvents = "none";
+              xo = x;
+            } else {
+              x = 0;
+            }
+          }
+        }
+      } else if (box.classList.contains("O")) {
+        wino.push(parseInt(box.dataset.str));
+        if (wino.length >= 3) {
+          for (let i = 0; i < winArray.length; i++) {
+            for (let j = 0; j < winArray[i].length; j++) {
+              for (let k = 0; k < wino.length; k++) {
+                if (wino[k] == winArray[i][j]) {
+                  o++;
+                }
+              }
+            }
+            if (o == 3) {
+              document.querySelector(
+                ".title"
+              ).innerHTML = `<span>O</span> Winner`;
+              document.body.style.pointerEvents = "none";
+              xo = o;
+            } else {
+              o = 0;
+            }
+          }
+        }
+      }
+      if (end == 9 && xo != 3) {
+        document.querySelector(
+          ".title"
+        ).innerHTML = `<span>There is no winner</span>`;
+      }
     });
   });
-  
 }
 play();
-
-//1 5 9
-
-//1 3 5 9
-
-
-
